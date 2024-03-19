67ceb39f2fd95676876883a23fb9cc93f9b9b540
Tapai Zoltan <zoltan@tapai.hu>
2024-03-19 11:51:01 +0100

    Kuponkereso page es laravel view, control, route kiszemetelve

--- resources/views/coupons/search.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layout')
-@section('content')
-<h1>Kuponok</h1>
-
-<div class="articlesContainer">
-    SIKER
-</div>
-
-@endsection
--- resources/views/filament/pages/coupon-search.blade.php
+++
@@ -1,8 +0,0 @@
-<x-filament-panels::page>
-
-    <x-filament-panels::form wire:submit='survey'>
-        {{$this->form}}
-        <x-filament-panels::form.actions :actions="$this->getFormActions()" />
-    </x-filament-panels::form>
-
-</x-filament-panels::page>
