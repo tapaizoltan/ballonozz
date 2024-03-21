c3c9b18418792dc9d74d9c37b2ba7c368a1f5119
Csikós Zsolt <csikoszs21@gmail.com>
2024-03-21 13:29:36 +0100

    fix: eldobjuk a `user_id` oszlopot a `coupons` táblából rollback esetén

--- database/migrations/2024_03_19_132138_add_field_to_coupons_table.php
+++ database/migrations/2024_03_19_132138_add_field_to_coupons_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('coupons', function (Blueprint $table) {
-            //
+            $table->dropColumn(['user_id']);
         });
     }
 };
