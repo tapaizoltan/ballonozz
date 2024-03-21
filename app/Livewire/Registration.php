95682222bc19e3a2f424b07659dc18ed47236c25
Csikós Zsolt <csikoszs21@gmail.com>
2024-03-21 13:24:59 +0100

    feat: regisztrációs komponens

---
+++ app/Livewire/Registration.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\User;
+use Livewire\Attributes\Validate;
+use Livewire\Component;
+
+class Registration extends Component
+{
+    public $name;
+    public $email;
+    public $phone;
+    public $password;
+    public $password_confirmation;
+    public $registered = false;
+
+    protected $rules = [
+        'name'     => 'required|min:6',
+        'email'    => 'required|email|unique:users',
+        'phone'    => 'required',
+        'password' => 'required|confirmed|min:6',
+    ];
+
+    public function registration()
+    {
+        $this->validate();
+
+        User::create([
+            'name'     => $this->name,
+            'email'    => $this->email,
+            'phone'    => $this->phone,
+            'password' => $this->password,
+        ]);
+
+        $this->registered = true;
+    }
+
+    public function render()
+    {
+        return view('livewire.registration');
+    }
+}
