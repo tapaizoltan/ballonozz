06752e91c3e3bee844f1ab9eba6650874f712c13
Csikós Zsolt <csikoszs21@gmail.com>
2024-03-25 14:16:56 +0100

    "naptár" rendezés, és (le)jelentkezés funkció megvalósítása

--- app/Filament/Pages/Checkin.php
+++ app/Filament/Pages/Checkin.php
@@ -5,6 +5,7 @@ namespace App\Filament\Pages;
 use App\Enums\AircraftLocationPilotStatus;
 use App\Models\AircraftLocationPilot;
 use App\Models\Coupon;
+use App\Models\Checkin as CheckinModel;
 use Filament\Pages\Page;
 use Livewire\Attributes\Computed;
 
@@ -12,29 +13,18 @@ class Checkin extends Page
 {
     public $coupons;
     public $coupon_id;
-    protected static ?string $title = 'Jelentkezések Page';
+    protected static ?string $title = 'Felhasználható kupon(ok)';
+    protected static ?string $navigationLabel = 'Jelentkezések';
     protected static ?string $navigationIcon = 'iconoir-user-badge-check';
     protected static string $view = 'filament.pages.checkin';
 
     public function mount()
     {
         $this->coupons = Coupon::query()->with('ticketType.aircrafts')->orderBy('source')->orderBy('coupon_code')->get();
+
         if ($this->coupons->count()) {
             $this->coupon_id = $this->coupons->first()->id;
         }
-    }
-
-    #[Computed]
-    public function dates()
-    {
-        if(!$this->coupon_id) {
-            return false;
-        }
-
-        return  AircraftLocationPilot::query()
-                    ->where('status', AircraftLocationPilotStatus::Published)
-                    ->whereIn('aircraft_id', $this->coupon->ticketType->aircrafts->pluck('id')->toArray())
-                    ->get();
     }
 
     #[Computed]
@@ -47,5 +37,35 @@ class Checkin extends Page
         return $this->coupons->find($this->coupon_id);
     }
 
-    
+    #[Computed]
+    public function dates()
+    {
+        if ($this->coupon === null) {
+            return false;
+        }
+
+        return AircraftLocationPilot::query()
+                ->where('status', AircraftLocationPilotStatus::Published)
+                ->whereIn('aircraft_id', $this->coupon->ticketType->aircrafts->pluck('id')->toArray())
+                ->orderBy('date')
+                ->orderBy('time')
+                ->get();
+    }
+
+    public function checkIn($aircraftLocationPilotId)
+    {
+        CheckinModel::create([
+            'aircraft_location_pilot_id' => $aircraftLocationPilotId,
+            'coupon_id'  => $this->coupon->id,
+            'status'     => 0,
+            'created_at' => now()->format('Y-m-d H:i:s')
+        ]);
+    }
+
+    public function checkOut($aircraftLocationPilotId)
+    {
+        CheckinModel::where('aircraft_location_pilot_id', $aircraftLocationPilotId)
+            ->where('coupon_id', $this->coupon->id)
+            ->delete();
+    }
 }
--- resources/views/filament/pages/checkin.blade.php
+++ resources/views/filament/pages/checkin.blade.php
@@ -1,38 +1,111 @@
 <x-filament-panels::page>
-    <div class="flex flex-wrap gap-5 w-full">
-        @foreach ($coupons as $coupon)
-            <div class="clickable card @if ($coupon->id === $coupon_id) selected @endif"
-                wire:click="$set('coupon_id', {{ $coupon->id }})">
-                <div class="font-semibold">{{ $coupon->coupon_code }}</div>
-                <div>
-                    <div>{{ $coupon->source }} kupon</div>
-                    <div>
-                        <div>
-                            <div>
-                                <span class="quantity">{{ $coupon->adult }} </span>
-                                <span class="quantity-description">felnőtt</span>
+    <div class="flex gap-5 p-2 overflow-x-auto">
+        @forelse ($coupons as $coupon)
+            @if ($coupon->isActive)
+                <div class="clickable card grid justify-between min-w-max @if ($coupon->id === $coupon_id) selected @endif"
+                    wire:click="$set('coupon_id', {{ $coupon->id }})">
+                    <div class="font-semibold">{{ $coupon->coupon_code }}</div>
+                    <div class="grid">
+                        <div>{{ $coupon->source }} kupon</div>
+                        <div class="description">{{ $coupon->ticketType?->name_stored_at_source ?? null }}</div>
+                        <div class="grid grid-cols-2 relative">
+                            
+                            <div class="flex flex-col justify-self-start">
+                                <div class="flex">
+                                    <span class="quantity">{{ $coupon->adult }} </span>
+                                    <span class="quantity-description">felnőtt</span>
+                                </div>
+                                <div class="flex">
+                                    <span class="quantity">{{ $coupon->children }} </span>
+                                    <span class="quantity-description">gyerek</span>
+                                </div>
                             </div>
-                            <div>
-                                <span class="quantity">{{ $coupon->children }} </span>
-                                <span class="quantity-description">gyerek</span>
+                            <div class="flex flex-col justify-self-end absolute top-1/2 translate-y-[-50%]">
+                                @if($coupon->private->value)    
+                                    @svg($coupon->private->getIcon())                             
+                                @endif
+                                @if ($coupon->vip->value)
+                                    @svg($coupon->vip->getIcon())
+                                @endif
                             </div>
-                        </div>
-                        <div>
-                            <!-- TODO SVG -->
                         </div>
                     </div>
                 </div>
+            @endif
+        @empty
+            <div class="card w-full">
+                <div class="flex justify-center">
+                    <x-heroicon-o-x-circle class="w-8 justify-self-end"/>
+                    <span class="text-lg ps-2 pt-0.5 justify-self-start">Jelenleg nincs kuponod. Nézz vissza később!</span>
+                </div>
             </div>
-        @endforeach
+        @endforelse
     </div>
 
     @if ($this->dates !== false)
-        <div class="card grid">
+        <div class="flex">
+            <h1 class="text-2xl font-bold tracking-tight text-gray-950 dark:text-white sm:text-3xl">{{ $this->coupon->coupon_code }}</h1>
+            <span class="pl-2 pt-1.5 sm:pt-2.5">{{ $this->coupon->source }} kupon</span>
+        </div>
+        <div class="flex gap-5 w-full p-2 overflow-x-auto">
             @forelse ($this->dates as $date)
-                <span>{{ $date->date }} {{ $date->time }}</span>
+                @php
+                    $checked = $date->isChecked($this->coupon->id)
+                @endphp
+                @if ($loop->first)
+                    @php
+                        $fly_at = $date->date;
+                    @endphp
+                    <div class="grid grid-flow-col gap-2">
+                        <div><div class="card max-h-min">
+                        
+                    <div class="pb-2">{{ Carbon\Carbon::parse($fly_at)->translatedFormat('Y F d.') }}</div>
+                @elseif($fly_at != $date->date && $fly_at != null)
+                    @php
+                        $fly_at = $date->date;
+                    @endphp
+                    </div></div></div><div class="grid grid-flow-col gap-2">
+                        <div>
+                        <div class="card max-h-min">
+                    <div class="pb-2">{{ Carbon\Carbon::parse($fly_at)->translatedFormat('Y F d.') }}</div>
+                @endif
+
+                <div class="card mb-4 grid gap-2 min-w-max border-2  @if($checked) border-green-500/80 @else dark:border-white/20 @endif">
+                    
+                    <div class="pt-1.5">{{ Carbon\Carbon::parse($date->time)->format('H:i') }}</div>
+                    
+                    <div class="flex gap-2">
+                        <x-heroicon-c-map-pin class="w-6 text-red-500"/>
+                        <span>{{ $date->location->name }}</span>
+                    </div>
+                    <div class="flex gap-2">
+                        <span class="text-blue-400">@svg($date->aircraft->type->getIcon())</span>
+                        <span>{{ $date->aircraft->name }}</span>
+                    </div>
+                    <div class="flex justify-between">
+                        <div class="flex text-zinc-400 justify-self-center">
+                            <x-heroicon-m-users class="w-5"/>
+                            <span class="ps-1 pt-2 text-sm font-semibold">{{ $date->coupons->sum('adult') + $date->coupons->sum('children') }}</span>
+                        </div>
+                        <div>
+                            @if(!$checked)
+                                <x-filament::button wire:click="checkIn({{ $date->id }})">Jelentkezem</x-filament::button>
+                            @else
+                                <x-filament::button class="!bg-red-600" wire:click="checkOut({{ $date->id }})">Lejelentkezem</x-filament::button>
+                            @endif
+                        </div>
+                    </div>
+                </div>
+                
             @empty
-                <span>Egyenlőre nincs kiírt repülés a(z) "{{ $this->coupon->coupon_code }}" kuponkódhoz. Nézz vissza később!</span>
+                <div class="card w-full">
+                    <div class="flex justify-center">
+                        <x-heroicon-o-x-circle class="w-8 justify-self-end"/>
+                        <span class="text-lg ps-2 pt-0.5 justify-self-start">Jelenleg nincs kuponod. Nézz vissza később!</span>
+                    </div>
+                </div>
             @endforelse
         </div>
+    </div>
     @endif
 </x-filament-panels::page>
