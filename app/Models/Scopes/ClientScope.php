5ebca03053587973ddc7554a01fa9e42ef8b76a6
Csikós Zsolt <csikoszs21@gmail.com>
2024-03-25 15:25:23 +0100

    feat: a felhasználó csak a saját kuponjait láthassa

---
+++ app/Models/Scopes/ClientScope.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models\Scopes;
+
+use App\Models\Coupon;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Scope;
+use Illuminate\Support\Facades\Auth;
+
+class ClientScope implements Scope
+{
+    /**
+     * Apply the scope to a given Eloquent query builder.
+     */
+    public function apply(Builder $builder, Model $model): void
+    {
+        if (!Auth::user()->hasRole(['admin'])) {
+            if ($model instanceof Coupon) {
+                $builder->where('user_id', Auth::id());
+            }
+        }
+    }
+}
