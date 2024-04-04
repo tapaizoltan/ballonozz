b6a775dfd3e26321b1eef55c4fb393be3e2b3d70
Csikós Zsolt <csikoszs21@gmail.com>
2024-04-04 16:52:20 +0200

    feat: jelentkezők listája véglegesítés form

--- app/Filament/Resources/AircraftLocationPilotResource/Pages/ListCheckins.php
+++ app/Filament/Resources/AircraftLocationPilotResource/Pages/ListCheckins.php
@@ -2,14 +2,15 @@
 
 namespace App\Filament\Resources\AircraftLocationPilotResource\Pages;
 
-use App\Enums\AircraftLocationPilotStatus;
 use App\Models\Coupon;
 use App\Models\Checkin;
 use Filament\Actions\Action;
 use Livewire\Attributes\Computed;
 use Filament\Resources\Pages\Page;
 use App\Models\AircraftLocationPilot;
+use Filament\Forms\Components\Select;
 use Filament\Notifications\Notification;
+use App\Enums\AircraftLocationPilotStatus;
 use App\Filament\Resources\AircraftLocationPilotResource;
 use Filament\Resources\Pages\Concerns\InteractsWithRecord;
 
@@ -18,9 +19,38 @@ class ListCheckins extends Page
     use InteractsWithRecord;
     protected static string $resource = AircraftLocationPilotResource::class;
     protected static string $view = 'filament.resources.aircraft-location-pilot-resource.pages.list-checkins';
+    protected static ?string $title = 'Jelentkezők';
     public $selectedCoupons = [];
     public $alreadyCheckedCoupons;
-    
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Action::make('finalize')->label('Véglegesít')
+                ->fillForm(['location_id' => $this->record->location_id])
+                ->form([
+                    Select::make('location_id')
+                        ->label('Helyszín')
+                        ->options($this->record->region->locations->pluck('name', 'id'))
+                        ->required(),       
+                ])
+                ->action(function (array $data): void {
+
+                    $deselectedCoupons = $this->record->coupons()->wherePivotNotIn('coupon_id', $this->selectedCoupons)->pluck('coupon_id')->toArray();
+                    $this->record->coupons()->updateExistingPivot($this->selectedCoupons, ['status' => 1]);
+                    $this->record->coupons()->updateExistingPivot($deselectedCoupons, ['status' => 0]);
+                    
+                    $data['status'] = AircraftLocationPilotStatus::Finalized;
+
+                    $this->record->update($data);
+
+                    Notification::make()
+                        ->success()
+                        ->title(__('filament-panels::resources/pages/edit-record.notifications.saved.title'))
+                        ->send();
+                })
+        ];
+    }
     public function mount(int | string $record): void
     {
         $this->record = $this->resolveRecord($record);
