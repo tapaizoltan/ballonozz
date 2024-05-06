edf0c9d70bccbadc9843dc02f4645520c3e27a35
Csikós Zsolt <csikoszs21@gmail.com>
2024-05-06 09:16:54 +0200

    feat: custom mail layout

---
+++ resources/views/components/layouts/mail.blade.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
+        <title>{{ config('app.name') }}</title>
+        <style>
+            body{
+                background-color: rgb(244 244 245);
+                font-family: 'Quicksand';
+            }
+            
+            .text-shadow {
+                filter: drop-shadow(0 2px 2px rgba(0,0,0,0.3));
+            }
+            
+            .btn {
+                text-decoration: none;
+                border-radius: 9999px;
+                padding-top: 0.375rem/* 6px */;
+                padding-bottom: 0.375rem/* 6px */;
+                padding-left: 1rem/* 16px */;
+                padding-right: 1rem/* 16px */;
+                filter: drop-shadow(0 2px 2px rgba(0,0,0,0.3));
+                text-transform: uppercase;
+            }
+            
+            .accent{
+                background-color: rgb(244 172 69);
+                color: rgb(255 255 255);
+                font-weight: 700;
+            }
+            .primary {
+                background-color: rgb(9 194 236);
+                color: rgb(255 255 255);
+                font-weight: 700;
+            }
+
+            .card{
+                margin-top: -0.5rem;
+                background-color: white; 
+                padding: 1.5rem;
+            }
+
+            .flip-hor-vert {
+                -moz-transform: scale(-1, -1);
+                -o-transform: scale(-1, -1);
+                -webkit-transform: scale(-1, -1);
+                transform: scale(-1, -1);
+            }
+
+            .container {
+                width: 100%;
+                margin-left: auto;
+                margin-right: auto;
+            }
+            @media (min-width: 640px) {
+                .container {
+                    max-width: 640px;
+                }
+            }
+            @media (min-width: 768px) {
+                .container {
+                    max-width: 768px;
+                }
+            }
+
+        </style>
+    </head>
+    <body>
+        <main class="container">
+            <svg xmlns="http://www.w3.org/2000/svg"  class="flip-hor-vert" viewbox="0 0 283.5 27.8" preserveAspectRatio="xMidYMax slice">
+                <path style="filter: invert(100%);"
+                    d="M0 0v6.7c1.9-.8 4.7-1.4 8.5-1 9.5 1.1 11.1 6 11.1 6s2.1-.7 4.3-.2c2.1.5 2.8 2.6 2.8 2.6s.2-.5 1.4-.7c1.2-.2 1.7.2 1.7.2s0-2.1 1.9-2.8c1.9-.7 3.6.7 3.6.7s.7-2.9 3.1-4.1 4.7 0 4.7 0 1.2-.5 2.4 0 1.7 1.4 1.7 1.4h1.4c.7 0 1.2.7 1.2.7s.8-1.8 4-2.2c3.5-.4 5.3 2.4 6.2 4.4.4-.4 1-.7 1.8-.9 2.8-.7 4 .7 4 .7s1.7-5 11.1-6c9.5-1.1 12.3 3.9 12.3 3.9s1.2-4.8 5.7-5.7c4.5-.9 6.8 1.8 6.8 1.8s.6-.6 1.5-.9c.9-.2 1.9-.2 1.9-.2s5.2-6.4 12.6-3.3c7.3 3.1 4.7 9 4.7 9s1.9-.9 4 0 2.8 2.4 2.8 2.4 1.9-1.2 4.5-1.2 4.3 1.2 4.3 1.2.2-1 1.4-1.7 2.1-.7 2.1-.7-.5-3.1 2.1-5.5 5.7-1.4 5.7-1.4 1.5-2.3 4.2-1.1c2.7 1.2 1.7 5.2 1.7 5.2s.3-.1 1.3.5c.5.4.8.8.9 1.1.5-1.4 2.4-5.8 8.4-4 7.1 2.1 3.5 8.9 3.5 8.9s.8-.4 2 0 1.1 1.1 1.1 1.1 1.1-1.1 2.3-1.1 2.1.5 2.1.5 1.9-3.6 6.2-1.2 1.9 6.4 1.9 6.4 2.6-2.4 7.4 0c3.4 1.7 3.9 4.9 3.9 4.9s3.3-6.9 10.4-7.9 11.5 2.6 11.5 2.6.8 0 1.2.2c.4.2.9.9.9.9s4.4-3.1 8.3.2c1.9 1.7 1.5 5 1.5 5s.3-1.1 1.6-1.4c1.3-.3 2.3.2 2.3.2s-.1-1.2.5-1.9 1.9-.9 1.9-.9-4.7-9.3 4.4-13.4c5.6-2.5 9.2.9 9.2.9s5-6.2 15.9-6.2 16.1 8.1 16.1 8.1.7-.2 1.6-.4V0H0z">
+                </path>
+            </svg>             
+            <div class="card">
+                {{ $slot }}
+            </div>
+            <svg xmlns="http://www.w3.org/2000/svg" viewbox="0 0 283.5 27.8" preserveAspectRatio="xMidYMax slice">
+                <path style="filter: invert(100%);"
+                    d="M0 0v6.7c1.9-.8 4.7-1.4 8.5-1 9.5 1.1 11.1 6 11.1 6s2.1-.7 4.3-.2c2.1.5 2.8 2.6 2.8 2.6s.2-.5 1.4-.7c1.2-.2 1.7.2 1.7.2s0-2.1 1.9-2.8c1.9-.7 3.6.7 3.6.7s.7-2.9 3.1-4.1 4.7 0 4.7 0 1.2-.5 2.4 0 1.7 1.4 1.7 1.4h1.4c.7 0 1.2.7 1.2.7s.8-1.8 4-2.2c3.5-.4 5.3 2.4 6.2 4.4.4-.4 1-.7 1.8-.9 2.8-.7 4 .7 4 .7s1.7-5 11.1-6c9.5-1.1 12.3 3.9 12.3 3.9s1.2-4.8 5.7-5.7c4.5-.9 6.8 1.8 6.8 1.8s.6-.6 1.5-.9c.9-.2 1.9-.2 1.9-.2s5.2-6.4 12.6-3.3c7.3 3.1 4.7 9 4.7 9s1.9-.9 4 0 2.8 2.4 2.8 2.4 1.9-1.2 4.5-1.2 4.3 1.2 4.3 1.2.2-1 1.4-1.7 2.1-.7 2.1-.7-.5-3.1 2.1-5.5 5.7-1.4 5.7-1.4 1.5-2.3 4.2-1.1c2.7 1.2 1.7 5.2 1.7 5.2s.3-.1 1.3.5c.5.4.8.8.9 1.1.5-1.4 2.4-5.8 8.4-4 7.1 2.1 3.5 8.9 3.5 8.9s.8-.4 2 0 1.1 1.1 1.1 1.1 1.1-1.1 2.3-1.1 2.1.5 2.1.5 1.9-3.6 6.2-1.2 1.9 6.4 1.9 6.4 2.6-2.4 7.4 0c3.4 1.7 3.9 4.9 3.9 4.9s3.3-6.9 10.4-7.9 11.5 2.6 11.5 2.6.8 0 1.2.2c.4.2.9.9.9.9s4.4-3.1 8.3.2c1.9 1.7 1.5 5 1.5 5s.3-1.1 1.6-1.4c1.3-.3 2.3.2 2.3.2s-.1-1.2.5-1.9 1.9-.9 1.9-.9-4.7-9.3 4.4-13.4c5.6-2.5 9.2.9 9.2.9s5-6.2 15.9-6.2 16.1 8.1 16.1 8.1.7-.2 1.6-.4V0H0z">
+                </path>
+            </svg>
+        </main>
+    </body>
+</html>
