36753fdf824152f205c5693f2ad89d4c9734b716
Csikós Zsolt <csikoszs21@gmail.com>
2024-05-09 09:00:52 +0200

    test: mail btn style

--- resources/views/mail/reset-password.blade.php
+++ resources/views/mail/reset-password.blade.php
@@ -5,7 +5,17 @@
     <div class="content">
         <p>Azért kapja ezt az üzenetet, mert a fiókjára jelszó helyreállítási kérés érkezett.</p>
         <div align="center" style="text-align: center;">
-            <a href="{{ $url }}" class="btn accent">
+            <a href="{{ $url }}" style="
+            text-decoration: none;
+            border-radius: 9999px;
+            padding-top: 6px;
+            padding-bottom: 6px;
+            padding-left: 16px;
+            padding-right: 16px;
+            background-color: #F4AC45;
+            color: #FFFFFF;
+            font-weight: 700;
+            ">
                 <span style="font-size: 18px; line-height: 28px;" class="text-shadow">JELSZÓ&nbsp;HELYREÁLLÍTÁS</span>
             </a>
         </div>
